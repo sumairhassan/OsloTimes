855487abfab0034bfe11aa84abd7362c1b179dd4
Sumair Hassan <sumairhassan10@gmail.com>
2021-01-01 11:50:11 +0100

    Second commit
    Route parameter

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,44 @@
-import React, {Component} from "react";
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   state = {
-    posts: []
-  }
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-     .then(res=>{
-       console.log(res)
-       this.setState({
-         posts: res.data.slice(0,10)
-       })
-     })
+    posts: [],
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      console.log(res);
+      this.setState({
+        posts: res.data.slice(0, 10),
+      });
+    });
   }
   render() {
-    const {posts } = this.state;
+    const { posts } = this.state;
     const postList = posts.length ? (
-      posts.map(post => {
-        return(
+      posts.map((post) => {
+        return (
           <div className="post card" key={post.id}>
             <div className="card-content">
-              <span className="card-title">{post.title}</span>
+              <Link to={"/" + post.id}>
+                <span className="card-title">{post.title}</span>
+              </Link>
               <p>{post.body}</p>
             </div>
-
           </div>
-        )
+        );
       })
     ) : (
       <div className="center"> No posts </div>
-    )
+    );
     return (
       <div className="container">
         <h4 className="center">Home</h4>
         {postList}
       </div>
     );
-};
-};
+  }
+}
 
 export default Home;
