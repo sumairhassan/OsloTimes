79b25848787e59248a7b26011c577e0fa0486978
Sumair Hassan <sumairhassan10@gmail.com>
2021-01-03 11:47:56 +0100

    Commit 4
    Map Dispatch To Props

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,12 +1,21 @@
 const initState = {
     posts: [
-        {id: '1', title: 'et iusto sed quo', body: 'iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut'},
-        {id: '2', title: 'sed quo iure voluptatem', body: ' occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut'},
-        {id: '3', title: 'iure voluptatem occaecati', body: ' omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut'}
+        {id: '1', title: '1. et iusto sed quo', body: 'iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut'},
+        {id: '2', title: '2. sed quo iure voluptatem', body: ' occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut'},
+        {id: '3', title: '3. iure voluptatem occaecati', body: ' omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut'}
     ]
 }
 
 const rootReducer = (state= initState, action) => {
+    if (action.type === 'DELETE_POST'){
+        let newPosts = state.posts.filter(post => {
+            return action.id !== post.id
+        });
+        return{
+            ... state,
+            posts: newPosts
+        }
+    }
     return state;
 }
 
